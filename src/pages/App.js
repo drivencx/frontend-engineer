f49901c802a63efe1190de1715bdb3ae854d5ab8
Ayrton Gomes <ayrtondcgomes@gmail.com>
2020-01-19 15:47:48 -0300

    [Feature] Base layout

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,9 +1,72 @@
 import React from 'react';
 
+import './app.scss';
+
+import drivenLogoSrc from 'assets/img/driven-logo.png';
+
 function App() {
   return (
     <div>
-      Initial
+
+      <header>
+        <div class="header-inner clearfix">
+          <div className="header-info">
+
+          </div>
+          <div className="header-content">
+            <div class="header-logo">
+              <a href="http://driven.cx/"><img alt="logo" src={drivenLogoSrc} /></a>
+            </div>
+            <div>
+              <div>
+                <div class="header-search">
+                  <div class="search">
+                    <i class="material-icons">search</i>
+                    <input type="search" name="search" placeholder="Search" />
+                  </div>
+                </div>
+                <div>
+                  <div>
+
+                  </div>
+                  <div>
+
+                  </div>
+                </div>
+              </div>
+              <div className="menu">
+                <ul className="nav-list">
+                  <li>
+                    <a href="#!">CATEGORIA</a>
+                  </li>
+                  <li>
+                    <a href="#!">CATEGORIA</a>
+                  </li>
+                  <li>
+                    <a href="#!">CATEGORIA</a>
+                  </li>
+                  <li>
+                    <a href="#!">CATEGORIA</a>
+                  </li>
+                  <li>
+                    <a href="#!">CATEGORIA</a>
+                  </li>
+                  <li>
+                    <a href="#!">CATEGORIA</a>
+                  </li>
+                  <li>
+                    <a href="#!">CATEGORIA</a>
+                  </li>
+                  <li>
+                    <a href="#!">CATEGORIA</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <main role="main"></main>
     </div>
   );
 }
