1ec399a61c78b152c5c2eac49271a68a9ce85015
Danilo Moraes <daniloomoraes@gmail.com>
2020-09-11 12:39:40 -0300

    Auto Complete

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -9,17 +9,17 @@ function closeNav() {
 
 // carousel cabeçalho
 $('.owl-carousel').owlCarousel({
-    loop:true,
+    loop: true,
     pagination: false,
     dots: false,
-    nav:false,
-    loop:true,
-    responsive:{
-        0:{
-            items:1,
+    nav: false,
+    loop: true,
+    responsive: {
+        0: {
+            items: 1,
         },
-        720:{
-            items:2,
+        720: {
+            items: 2,
         }
     }
 })
@@ -27,11 +27,33 @@ $('.owl-carousel').owlCarousel({
 // carrinho cabeçalho
 function OpenMyCart() {
     document.getElementById("MyCartDropdown").classList.toggle("exibir-area-do-carrinho");
-  }
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(event) {
+}
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
     if (!event.target.matches('.dropbtn')) {
         var dropdowns = document.getElementsByClassName("area-do-carrinho-content");
     }
-  }
-  
+}
+
+// busca autocomplete
+var options = {
+    url: function (phrase) {
+        return "http://agenciabluefoot.vtexcommercestable.com.br/buscaautocomplete/?productNameContains=" + phrase + "&format=json";
+        alert ("" + phrase + "");
+    },
+    listLocation: "itemsReturned",
+    getValue: "name",
+    template: {
+        type: "iconLeft",
+        fields: {
+            iconSrc: "thumbUrl"
+        }
+    },
+    list: {
+        match: {
+            enabled: true
+        }
+    },
+};
+
+$(".busca-produtos").easyAutocomplete(options);
